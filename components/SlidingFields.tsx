d5060b150845d9239919ed712393131b655d6daa
Kelly Nezat <Soullab1@gmail.com>
2025-09-19 17:51:50 -0400

    🐛 Fix MayaChatInterface → MaiaChatInterface in SlidingFields

--- components/SlidingFields.tsx
+++ components/SlidingFields.tsx
@@ -157,7 +157,7 @@ export default function SlidingFields() {
   // Center view - Maya Chat
   const renderCenterView = () => (
     <div className="w-full h-full relative">
-      <MayaChatInterface />
+      <MaiaChatInterface />
       
       {/* Gentle slide hints */}
       <div className="fixed bottom-20 left-1/2 transform -translate-x-1/2 z-10">
